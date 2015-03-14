4c44d1f03f3f878b7f79497688465700df0a149d
amwhenderson <amwhenderson@gmail.com>
2015-03-14 17:07:42 -0400

    First commit

---
+++ site/accounts/amwhenderson.php
@@ -0,0 +1,9 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: amwhenderson
+email: amwhenderson@gmail.com
+password: >
+  $2a$10$7wcRXWb1OfOojMGWla8c7epY5jtqGcBd0TW9pwSmUsJZDchNovcC6
+language: en
+role: admin
+token: 46eb590ecc5f4fd513e19235028f1b69628c4bf6
